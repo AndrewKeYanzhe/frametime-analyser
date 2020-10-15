f29b14fcd60d9772e62d84a632ba11ae0b1681c4
AndrewKeYanzhe <keyanzhe@gmail.com>
2020-10-15 20:54:09 +0800

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-vid = "F:/ReLive/2020.10.14-21.51.mp4"
-
-from moviepy.editor import VideoFileClip
-filepath = "output.avi"
-clip = VideoFileClip(vid)
-
-for f in clip.iter_frames(True):
-    np_array_frame = f
